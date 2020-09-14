c578ba9d56c93e3ba77493f70947825e997d6351
Jeyson Fuentes Alfaro <jfuentes@dcp.pe>
2020-09-14 15:02:21 -0500

    First Commit

---
+++ config.js
@@ -0,0 +1,14 @@
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  }
+
+  // Exports
+  module.exports = firebaseConfig;
